98434eee5d5137e085b416fd6ee6642e50744bdb
재준 정 <hosun0507@gmail.com>
2020-12-15 06:47:01 +0900

    거꾸로 묻는거 질문 함수 추가, 답에 띄어쓰기까지 같이 입력되도록 수정

--- 물음에 답하기.c
+++ 물음에 답하기.c
@@ -4,11 +4,13 @@
 #include<string.h> // strcpy와 strtok 함수가 선언된 헤더 파일
 #include<stdlib.h> // rnad와 srand 함수가 선언된 헤더 파일
 
-int main_b() {
-	char question[12][80] = { "When can i a copy?", "What is the best way to go to the food store?","Why are you late?","It's cold isn't it?","Can i fill out this application?","What causing the noise upstairs?","Will you order supplies tommorow?","My laptop won't start?","I'll send you a price list?","How long have you worked at our company?","We have a meeting at two","Escuse me, i'm looking for a book colled communication by Rachel Evans?" };
-	char r_answer[12][80] = { "On the second floor","Take highway 52","I missed the bus","Yes, you should wear swearter","Sure, no problem","The neighbor are moving the furniture","It was a surprise","Maybe the battery's out","That'll be great,thank you", "For five years","Realy? I didn't know that","I'm sorry, we don't have that book, I'll order it for you" };
-	char answer[71], storage[71];
+char question[12][80] = { "When can i a copy?", "What is the best way to go to the food store?","Why are you late?","It's cold isn't it?","Can i fill out this application?","What causing the noise upstairs?","Will you order supplies tommorow?","My laptop won't start?","I'll send you a price list?","How long have you worked at our company?","We have a meeting at two","Escuse me, i'm looking for a book colled communication by Rachel Evans?" };
+char r_answer[12][80] = { "On the second floor","Take highway 52","I missed the bus","Yes, you should wear swearter","Sure, no problem","The neighbors are moving the furniture","I did it yesterday","Maybe the battery's out","That'll be great,thank you", "For five years","Realy? I didn't know that","I'm sorry, we don't have that book, I'll order it for you" };
+char answer[71], storage[71];
 
+
+int main_b() {
+	
 	srand(time(NULL)); // 난수 기준값 변경
 	for (int i = 11; i > 0; i--)
 	{
@@ -22,7 +24,7 @@ int main_b() {
 		strcpy(r_answer[randdom], storage); // 질문과 같은 위치에 답 위치 변경
 
 		printf("%s ", question[i]);
-		scanf_s("%s", &answer, 71);
+		gets(answer); // 엔터를 제외하고 모든 문자 입력
 		printf("%s \n", r_answer[i]);
 
 
@@ -30,6 +32,28 @@ int main_b() {
 	}
 
 
-	printf("안녕");
+	printf("\n \n \n \n \n \n");
 	return 0;
+}
+int main_b2() {
+	srand(time(NULL)); // 난수 기준값 변경
+	for (int i = 11; i > 0; i--)
+	{
+		int randdom = rand() % i + 1; // 0~i까지 난수 발생
+		strcpy(storage, r_answer[i]);
+		strcpy(r_answer[i], r_answer[randdom]);
+		strcpy(r_answer[randdom], storage);  // 질문 위치 변경
+
+		strcpy(storage, question[i]);
+		strcpy(question[i], question[randdom]);
+		strcpy(question[randdom], storage); // 질문과 같은 위치에 답 위치 변경
+
+		printf("%s ", r_answer[i]);
+		gets(answer); // 엔터를 제외하고 모든 문자 입력
+		printf("%s \n", question[i]);
+
+		
+
+	}
+	printf("\n \n \n \n \n \n");
 }
